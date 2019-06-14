abe59e75b7566bc353f3a953dbabd514794d4bb0
Troy Williams <troy.williams@learningworks.co.nz>
2019-06-15 09:24:30 +1200

    Add create measure to item factory.

--- classes/local/factories/item.php
+++ classes/local/factories/item.php
@@ -19,8 +19,10 @@ namespace report_lp\local\factories;
 defined('MOODLE_INTERNAL') || die();
 
 use stdClass;
+use coding_exception;
 use report_lp\local\measures_list;
 use report_lp\local\grouping;
+use report_lp\local\measure;
 use report_lp\local\persistents\item_configuration;
 
 class item {
@@ -34,17 +36,54 @@ class item {
         $this->measureslist = $measureslist;
     }
 
-    public function create_grouping(int $id = 0, stdClass $record = null) {
+    /**
+     * Build a grouping, either new or load existing.
+     *
+     * @param int $id
+     * @param stdClass|null $record
+     * @return grouping
+     * @throws \ReflectionException
+     * @throws coding_exception
+     */
+    public function create_grouping(int $id = 0, stdClass $record = null) : grouping {
         $grouping = new grouping();
         $itemconfiguration = new item_configuration($id, $record);
         if ($id <= 0) {
             $itemconfiguration->set('courseid', $this->course->id);
+            $itemconfiguration->set('classname', $grouping->get_class_name());
+            $itemconfiguration->set('shortname', $grouping->get_short_name());
+            $itemconfiguration->set('isgrouping', 1);
         }
-        $itemconfiguration->set('classname', $grouping->get_class_name());
-        $itemconfiguration->set('shortname', $grouping->get_short_name());
-        $itemconfiguration->set('isgrouping', 1);
         $grouping->set_configuration($itemconfiguration);
         return $grouping;
+    }
+
+    /**
+     * Build a measure, either new or load existing.
+     *
+     * @param int $id
+     * @param stdClass|null $record
+     * @param string|null $shortname
+     * @return measure
+     * @throws \ReflectionException
+     * @throws coding_exception
+     */
+    public function create_measure(int $id = 0, stdClass $record = null, string $shortname = null) : measure  {
+        $itemconfiguration = new item_configuration($id, $record);
+        if ($id <= 0) {
+            if (is_null($shortname)) {
+                throw new coding_exception("Creating a brand new measure require class shortname to be passed");
+            }
+            $measure = $this->measureslist->find_by_short_name($shortname);
+            $itemconfiguration->set('courseid', $this->course->id);
+            $itemconfiguration->set('classname', $measure->get_class_name());
+            $itemconfiguration->set('shortname', $measure->get_short_name());
+            $itemconfiguration->set('isgrouping', 1);
+        } else {
+            $measure = $this->measureslist->find_by_short_name($itemconfiguration->get('shortname'));
+        }
+        $measure->set_configuration($itemconfiguration);
+        return $measure;
     }
 
 }
