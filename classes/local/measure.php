8211170ea1ceec51ae2c62598ea50e9973d9c02d
Troy Williams <troy.williams@learningworks.co.nz>
2019-06-17 09:59:23 +1200

    Define new method get data for users for concretes.

--- classes/local/measure.php
+++ classes/local/measure.php
@@ -25,6 +25,12 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright   2019 Troy Williams <troy.williams@learningworks.co.nz>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 abstract class measure extends item {
+
+    /**
+     * @param userlist $userlist
+     * @return array|null
+     */
+    abstract public function get_data_for_users(userlist $userlist) : ? array;
+
 }
