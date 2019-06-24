44b67b93ecaa5908ae5ef30a9cbd7e493e576125
Troy Williams <troy.williams@learningworks.co.nz>
2019-06-24 15:56:40 +1200

    Delete item support.

--- configure.php
+++ configure.php
@@ -18,17 +18,84 @@ require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/report/lp/lib.php');
 
 $courseid = required_param('courseid', PARAM_INT);
-$params = ['id' => $courseid];
-$course = $DB->get_record('course', $params, '*', MUST_EXIST);
-$context = context_course::instance($course->id, MUST_EXIST);
-$PAGE->set_context($context);
+$itemid = optional_param('itemid', 0, PARAM_INT);
+$action = optional_param('action', '', PARAM_ALPHA);
+$confirmed = optional_param('confirmed', 0, PARAM_INT);
+
+$course = $DB->get_record('course', ['id' => $courseid], '*', MUST_EXIST);
+$systemcontext = context_system::instance();
+$coursecontext = context_course::instance($course->id, MUST_EXIST);
+
+$reportconfiguration = report_lp\local\persistents\report_configuration::get_record(['courseid' => $courseid]);
+// Setup a new report configuration instance.
+if (false === $reportconfiguration) {
+    $reportconfiguration = new report_lp\local\persistents\report_configuration();
+    $reportconfiguration->set('courseid', $courseid);
+    $reportconfiguration->save();
+}
+$PAGE->set_context($systemcontext);
 require_login($course);
-$url = new moodle_url('/report/lp/configure.php', ['courseid' => $courseid]);
+require_capability('report/lp:configure', $systemcontext);
+$url = report_lp\local\factories\url::get_config_url($course);
 $PAGE->set_url($url);
+$css = new moodle_url('/report/lp/scss/styles.css');
+$PAGE->requires->css($css);
 $measures = report_lp_get_supported_measures();
 $measurelist = new report_lp\local\measure_list($measures);
+$itemfactory = new report_lp\local\factories\item($course, $measurelist);
 $itemtree = new report_lp\local\item_tree($course, $measurelist);
 $renderer = $PAGE->get_renderer('report_lp');
+switch ($action) {
+    case 'moveup':
+        if (confirm_sesskey()) {
+            if (empty($itemid)) {
+                throw new moodle_exception('Empty itemid');
+            }
+        }
+        redirect($pageurl);
+        break;
+    case 'movedown':
+        if (confirm_sesskey()) {
+            if (empty($itemid)) {
+                throw new moodle_exception('Empty itemid');
+            }
+        }
+        redirect($pageurl);
+        break;
+    case 'delete':
+        if (confirm_sesskey()) {
+            if (empty($itemid)) {
+                throw new moodle_exception('Empty itemid');
+            }
+            $item = $itemfactory->create_item(new report_lp\local\persistents\item_configuration($itemid));
+            if ($confirmed) {
+                $configuration = $item->get_configuration();
+                if ($item instanceof report_lp\local\grouping) {
+                    $children = $configuration->get_children();
+                    foreach ($children as $child) {
+                        $child->delete();
+                    }
+                }
+                $configuration->delete();
+                redirect($url);
+            }
+            if ($item instanceof report_lp\local\grouping) {
+                $message = get_string('deletegroupingitem', 'report_lp', $item->get_label());
+            } else {
+                $message  = get_string('deletemeasureitem', 'report_lp', $item->get_label());
+            }
+            echo $OUTPUT->header();
+            echo $OUTPUT->heading(get_string('configurereportfor', 'report_lp', $course->fullname));
+            $continueurl = report_lp\local\factories\url::get_item_action_url($courseid, $itemid, 'delete');
+            $continueurl->param('confirmed', 1);
+            $continuebutton = new single_button($continueurl, get_string('delete'), 'post');
+            echo $OUTPUT->confirm($message, $continuebutton, $url);
+            echo $OUTPUT->footer();
+            exit;
+        }
+        redirect($pageurl);
+        break;
+}
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('configurereportfor', 'report_lp', $course->fullname));
 echo $renderer->render(new report_lp\output\add_item_menu($course, $measurelist));
@@ -36,8 +103,5 @@ if (count($itemtree) == 0) {
     echo $renderer->render(new report_lp\output\no_items_configured());
 } else {
     echo $renderer->render(new report_lp\output\item_tree_configuration($itemtree));
-    // For debugging as build template.
-    $out = new report_lp\output\item_tree_configuration($itemtree);
-    print_object($out->export_for_template($renderer));
 }
 echo $OUTPUT->footer();
