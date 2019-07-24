469747a9455c96cc3f3d9a2be96fcbb8e9d88258
Troy Williams <troy.williams@learningworks.co.nz>
2019-07-24 17:34:17 +1200

    Add grouping item visitor.

---
+++ classes/local/visitors/grouping_item_visitor.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace report_lp\local\visitors;
+
+defined('MOODLE_INTERNAL') || die();
+
+use report_lp\local\grouping;
+use report_lp\local\item;
+use report_lp\local\item_visitor;
+
+class grouping_item_visitor implements item_visitor {
+
+    public function visit(item $item) {
+        $items = [];
+        if ($item instanceof grouping) {
+            $items[] = $item;
+        }
+        foreach ($item->getChildren() as $child) {
+            if ($item instanceof grouping) {
+                $item = array_merge(
+                    $items,
+                    $child->accept($this)
+                );
+            }
+        }
+        return $items;
+    }
+
+}
