72a060428a5c725b82e5b8f9db630fa22b13efe7
Troy Williams <troy.williams@learningworks.co.nz>
2019-07-25 16:03:26 +1200

    Add new get and create methods.

--- classes/local/factories/item.php
+++ classes/local/factories/item.php
@@ -18,11 +18,14 @@ namespace report_lp\local\factories;
 
 defined('MOODLE_INTERNAL') || die();
 
+use report_lp\local\learner_information_grouping;
 use stdClass;
 use coding_exception;
 use moodle_exception;
 use report_lp\local\grouping;
 use report_lp\local\item_type_list;
+use report_lp\local\learner;
+use report_lp\local\learner_field;
 use report_lp\local\measure;
 use report_lp\local\persistents\item_configuration;
 
@@ -80,31 +83,132 @@ class item {
     }
 
     /**
-     * Get root grouping, always create if non existent by default.
+     * Get root grouping. Optional parameter to create configuration record.
      *
      * @param bool $create
      * @return grouping
      * @throws \ReflectionException
      * @throws coding_exception
      */
-    public function get_root_grouping(bool $create = true) : grouping {
+    public function get_root_grouping(bool $create = false) : grouping {
         $grouping = $this->get_class_instance_from_list('grouping');
         $rootconfiguration = item_configuration::get_record(
             ['courseid' => $this->course->id, 'parentitemid' => 0]
         );
         if (!$rootconfiguration) {
-            if (!$create) {
-                throw new coding_exception('Root grouping configuration does not exist');
-            }
             $rootconfiguration = new item_configuration();
             $rootconfiguration->set('courseid', $this->course->id);
-            $rootconfiguration->set('classname', grouping::get_class_name());
-            $rootconfiguration->set('shortname', grouping::get_short_name());
+            $rootconfiguration->set('classname', $grouping::get_class_name());
+            $rootconfiguration->set('shortname', $grouping::get_short_name());
             $rootconfiguration->set('usecustomlabel', 1);
             $rootconfiguration->set('customlabel', format_text($this->course->fullname, FORMAT_PLAIN));
-            $rootconfiguration->save();
+            $rootconfiguration->set('parentitemid', 0);
+            $rootconfiguration->set('depth', 0);
+            $rootconfiguration->set('islocked', 1);
+            if ($create) {
+                $rootconfiguration->save();
+            }
         }
         $grouping->load_configuration($rootconfiguration);
+        return $grouping;
+    }
+
+    /**
+     * Quick method of creating a grouping.
+     *
+     * @param string $customlabel
+     * @param bool $islocked
+     * @param int $parentitemid
+     * @return grouping
+     * @throws \ReflectionException
+     * @throws \core\invalid_persistent_exception
+     * @throws coding_exception
+     */
+    public function create_grouping(string $customlabel = '', bool $islocked = false, int $parentitemid = 0) : grouping {
+        if ($parentitemid < 1) {
+            throw new coding_exception("Invalid parent item id");
+        }
+        $grouping = $this->get_class_instance_from_list('grouping');
+        $configuration = new item_configuration();
+        $configuration->set('courseid', $this->course->id);
+        $configuration->set('classname', $grouping::get_class_name());
+        $configuration->set('shortname', $grouping::get_short_name());
+        if (!empty($customlabel)) {
+            $configuration->set('usecustomlabel', 1);
+            $configuration->set('customlabel', format_text($customlabel, FORMAT_PLAIN));
+        }
+        $configuration->set('parentitemid', $parentitemid);
+        $configuration->set('islocked', $islocked);
+        $configuration->create();
+        $grouping->load_configuration($configuration);
+        return $grouping;
+    }
+
+    /**
+     * Create the learner item.
+     *
+     * @param int $parentitemid
+     * @return learner
+     * @throws \ReflectionException
+     * @throws \core\invalid_persistent_exception
+     * @throws coding_exception
+     */
+    public function create_learner(int $parentitemid) : learner {
+        $learner = $this->get_class_instance_from_list('learner');
+        $configuration = new item_configuration();
+        $configuration->set('courseid', $this->course->id);
+        $configuration->set('classname', $learner::get_class_name());
+        $configuration->set('shortname', $learner::get_short_name());
+        $configuration->set('parentitemid', $parentitemid);
+        $configuration->set('islocked', true);
+        $configuration->create();
+        $learner->load_configuration($configuration);
+        return $learner;
+    }
+
+    /**
+     * @param $shortname
+     * @param int $parentitemid
+     * @return learner_field
+     * @throws \ReflectionException
+     * @throws \core\invalid_persistent_exception
+     * @throws coding_exception
+     */
+    public function create_learner_field($shortname, int $parentitemid) : learner_field {
+        if ($parentitemid < 1) {
+            throw new coding_exception("Invalid parent item id");
+        }
+        $learnerfield = $this->get_class_instance_from_list($shortname);
+        $configuration = new item_configuration();
+        $configuration->set('courseid', $this->course->id);
+        $configuration->set('classname', $learnerfield::get_class_name());
+        $configuration->set('shortname', $learnerfield::get_short_name());
+        $configuration->set('parentitemid', $parentitemid);
+        $configuration->create();
+        $learnerfield->load_configuration($configuration);
+        return $learnerfield;
+    }
+
+    /**
+     * @param int $parentitemid
+     * @return learner_information_grouping
+     * @throws \ReflectionException
+     * @throws \core\invalid_persistent_exception
+     * @throws coding_exception
+     */
+    public function create_learner_information_grouping(int $parentitemid) : learner_information_grouping {
+        if ($parentitemid < 1) {
+            throw new coding_exception("Invalid parent item id");
+        }
+        $grouping = $this->get_class_instance_from_list('learner_information_grouping');
+        $configuration = new item_configuration();
+        $configuration->set('courseid', $this->course->id);
+        $configuration->set('classname', $grouping::get_class_name());
+        $configuration->set('shortname', $grouping::get_short_name());
+        $configuration->set('parentitemid', $parentitemid);
+        $configuration->set('islocked', true);
+        $configuration->create();
+        $grouping->load_configuration($configuration);
         return $grouping;
     }
 
@@ -216,20 +320,19 @@ class item {
      * Get all groupings for the course.
      *
      * @return array
-     * @throws \ReflectionException
      * @throws coding_exception
      */
-    public function get_groupings() {
+    public function get_grouping_from_item_configurations() {
         $groupings = [];
         $itemconfigurations = item_configuration::get_records(
             ['courseid' => $this->course->id]
         );
         foreach ($itemconfigurations as $itemconfiguration) {
+            $item = $this->get_item_from_persistent($itemconfiguration);
             // Only want groupings.
-            if (grouping::get_short_name() != $itemconfiguration->get('shortname')) {
-                continue;
+            if ($item instanceof grouping) {
+                $groupings[] = $item;
             }
-            $groupings[] = $this->get_grouping($itemconfiguration);
         }
         return $groupings;
     }
