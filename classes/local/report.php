614b19b8951230f350eee84da8cdeb66a6e9e985
Troy Williams <troy.williams@learningworks.co.nz>
2019-07-25 16:40:50 +1200

    A class called API.

---
+++ classes/local/report.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace report_lp\local;
+
+defined('MOODLE_INTERNAL') || die();
+
+use stdClass;
+use report_lp\local\persistents\report_configuration;
+
+/**
+ * General API methods.
+ *
+ * @package     report_lp
+ * @copyright   2019 Troy Williams <troy.williams@learningworks.co.nz>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class report {
+
+    public static function create_course_instance(stdClass $course) {
+        $itemfactory = new factories\item($course, new item_type_list());
+        $root = $itemfactory->get_root_grouping(true);
+        $learnergrouping = $itemfactory->create_learner_information_grouping($root->get_id());
+        $learneritem = $itemfactory->create_learner($learnergrouping->get_id());
+        $idnumberfield = $itemfactory->create_learner_field('idnumber_learner_field', $learnergrouping->get_id());
+        $idnumberfield->get_configuration()->set('islocked', true)->save();
+        $coursegroupsfield = $itemfactory->create_learner_field('course_groups_learner_field', $learnergrouping->get_id());
+        $coursegroupsfield->get_configuration()->set('islocked', true)->save();
+        $reportconfiguration = new report_configuration();
+        $reportconfiguration->set('courseid', $course->id);
+        $reportconfiguration->set('enabled', true);
+        $reportconfiguration->create();
+        return $reportconfiguration;
+    }
+
+    public static function delete_course_instance(stdClass $course) {
+        global $DB;
+        $DB->delete_records('report_lp', ['courseid' => $course->id]);
+        $DB->delete_records('report_lp_items', ['courseid' => $course->id]);
+    }
+
+}
