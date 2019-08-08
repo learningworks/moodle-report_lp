73935116be7a600c280ca208d39a1f1cde5b84ba
Troy Williams <troy.williams@learningworks.co.nz>
2019-08-08 16:38:20 +1200

    Add pre order visitor.

---
+++ classes/local/visitors/pre_order_visitor.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace report_lp\local\visitors;
+
+defined('MOODLE_INTERNAL') || die();
+
+use report_lp\local\grouping;
+use report_lp\local\item;
+use report_lp\local\contracts\item_visitor;
+
+class pre_order_visitor implements item_visitor {
+
+    /**
+     * Visit.
+     *
+     * @param item $item
+     * @return array|mixed
+     */
+    public function visit(item $item) {
+        $items = [
+            $item,
+        ];
+        if ($item instanceof grouping) {
+            foreach ($item->get_children() as $child) {
+                $items = array_merge(
+                    $items,
+                    $child->accept($this)
+                );
+            }
+        }
+        return $items;
+    }
+
+}
