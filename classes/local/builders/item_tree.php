d6d24342e1c43fe71fdea6055d7083e620a700a5
Troy Williams <troy.williams@learningworks.co.nz>
2019-08-08 15:01:03 +1200

    Ensure item is enabled.

--- classes/local/builders/item_tree.php
+++ classes/local/builders/item_tree.php
@@ -18,13 +18,10 @@ namespace report_lp\local\builders;
 
 defined('MOODLE_INTERNAL') || die();
 
-use ArrayIterator;
-use Countable;
-use IteratorAggregate;
+use coding_exception;
 use report_lp\local\factories\item as item_factory;
 use report_lp\local\grouping;
 use report_lp\local\item_type_list;
-use report_lp\local\persistents\item_configuration;
 use stdClass;
 
 /**
@@ -36,75 +33,65 @@ use stdClass;
  */
 class item_tree {
 
-    private $depth = 0;
-
-    /**
-     * @var stdClass $course Course object.
-     */
+    /** @var stdClass $course Course object. */
     private $course;
 
-    /** @var array $items One dimensional array of all items. */
-    private $items = [];
+    /** @var item_type_list  */
+    private $itemtypelist;
 
     /** @var item_factory $itemfactory Builds items based on configuration. */
     private $itemfactory;
-
-    /** @var grouping $tree Tree struction, parents and children. */
-    private $tree;
 
     /**
      * item_tree constructor.
      *
      * @param stdClass $course
-     * @param item_type_list|null $itemtypelist
-     * @throws \ReflectionException
-     * @throws \coding_exception
+     * @param item_type_list $itemtypelist
      */
-    public function __construct(stdClass $course, item_type_list $itemtypelist = null) {
+    public function __construct(stdClass $course, item_type_list $itemtypelist) {
         $this->course = $course;
-        if (is_null($itemtypelist)) {
-            $itemtypelist = new item_type_list();
-        }
-        $this->itemfactory = new item_factory($this->course, $itemtypelist);
+        $this->itemtypelist = $itemtypelist;
+        $this->itemfactory = new item_factory($this->course, $this->itemtypelist);
     }
 
     /**
-     * Build the tree based on a ordered collection of item configurations.
+     * Build tree from array of item persistents.
      *
+     * @param array $itemconfigurations
      * @return grouping|null
-     * @throws \coding_exception
-     * @throws \dml_exception
+     * @throws coding_exception
      */
-    public function build_from_item_configurations() : ? grouping {
-        $itemconfigurations = item_configuration::get_ordered_items($this->course->id);
-        if ($itemconfigurations) {
-            foreach ($itemconfigurations as $itemconfiguration) {
-                $item = $this->itemfactory->get_item_from_persistent($itemconfiguration);
-                $this->items[$item->get_id()] = $item;
+    public function build_from_item_configurations(array $itemconfigurations) : ? grouping {
+        $root = null;
+        $items = [];
+        foreach ($itemconfigurations as $itemconfiguration) {
+            $item = $this->itemfactory->get_item_from_persistent($itemconfiguration);
+            if ($this->course->id != $item->get_courseid()) {
+                continue;
             }
-            foreach ($this->items as $item) {
-                if ($item->is_root()) {
-                    $this->tree = $item;
-                } else {
-                    $parentitem = $this->items[$item->get_parentitemid()];
-                    /** @var grouping $parentitem */
-                    $parentitem->add_item($item);
-                    if ($item->get_depth() > $this->depth) {
-                        $this->depth = $item->get_depth();
-                    }
-                }
+            if ($item->is_enabled()) {
+                $items[$item->get_id()] = $item;
             }
         }
-        return $this->tree;
-    }
-
-    /**
-     * Get the current depth of the tree.
-     *
-     * @return int
-     */
-    public function get_current_depth() {
-        return (int) $this->depth;
+        // Ensure array sorted by depth and then order and maintain keys.
+        uasort($items, function($a, $b) {
+            $sort = $a->get_depth() <=> $b->get_depth();
+            if ($sort == 0) {
+                $sort = $a->get_sortorder() <=> $b->get_sortorder();
+            }
+            return $sort;
+        });
+        // Maybe a better way, but this works for now.
+        foreach ($items as $item) {
+            if ($item->is_root()) {
+                $root = $item;
+            } else {
+                $parentitem = $items[$item->get_parentitemid()];
+                /** @var grouping $parentitem */
+                $parentitem->add_item($item);
+            }
+        }
+        return $root;
     }
 
 }
